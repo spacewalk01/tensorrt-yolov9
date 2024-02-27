7dc6463ad7506809b8e910327b46ddf647952a94
spacewalk01 <selmee006@gmail.com>
2024-02-28 02:52:12 +0900

    Update yolov9.h

--- src/yolov9.h
+++ src/yolov9.h
@@ -11,15 +11,7 @@ struct Detection
 {
     float confidence;
     int class_id;
-    cv::Rect box;
-};
-
-struct YoloParams
-{
-    float confThreshold = 0.3;
-    float nmsThreshold = 0.4;
-    int kMaxInputImageSize = 4096 * 4096;
-
+    Rect box;
 };
 
 class Yolov9
@@ -27,38 +19,36 @@ class Yolov9
 
 public:
 
-    Yolov9(string modelPath);
-
-    void predict(Mat& inputImage, std::vector<Detection>& bboxes);
-
+    Yolov9(string engine_path);
+    
     ~Yolov9();
+    
+    void predict(Mat& image, std::vector<Detection>& boxes);
 
-    void draw(cv::Mat& testImg, const std::vector<Detection>& bboxes);
+    void draw(Mat& image, const std::vector<Detection>& boxes);
+
+private:
+
+    size_t get_dim(const Dims& dims);
+
+    void postprocess(vector<Detection>& boxes);
 
 private:
 
-    YoloParams mParams;
-
-    void deserializeEngine(string enginePath);
-
-    void initialize();
-
-    size_t getSizeByDim(const Dims& dims);
-
-    void postprocess(std::vector<Detection>& bboxes);
-
-private:
-    std::vector<cv::Scalar> colors;
-
-    vector<Dims> mInputDims;            //!< The dimensions of the input to the network.
-    vector<Dims> mOutputDims;           //!< The dimensions of the output to the network.
-    vector<float*> mGpuBuffers;          //!< The vector of device buffers needed for engine execution
-    vector<float*> mCpuBuffers;
-    vector<size_t> mBufferBindingBytes;
-    vector<size_t> mBufferBindingSizes;
-    cudaStream_t mCudaStream;
-
-    IRuntime* mRuntime;                 //!< The TensorRT runtime used to deserialize the engine
-    ICudaEngine* mEngine;               //!< The TensorRT engine used to run the network
-    IExecutionContext* mContext;        //!< The context for executing inference using an ICudaEngine
+    vector<Scalar> colors;
+    vector<float*> gpu_buffers;          //!< The vector of device buffers needed for engine execution
+    vector<float*> cpu_buffers;
+    
+    int input_h;
+    int input_w;
+    int num_output_boxes;
+    int output_size;
+    const int MAX_INPUT_SIZE = 4096 * 4096;
+    float confThreshold = 0.3;
+    float nmsThreshold = 0.4;
+    
+    cudaStream_t cuda_stream;
+    IRuntime* runtime;                 //!< The TensorRT runtime used to deserialize the engine
+    ICudaEngine* engine;               //!< The TensorRT engine used to run the network
+    IExecutionContext* context;        //!< The context for executing inference using an ICudaEngine
 };
